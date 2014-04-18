30ad4a8884819240f86f5c5b968581a08e4057bd
Eugene <Dolgy>
2014-04-18 16:16:27 +0600

    0.1.0

---
+++ git-bump.cmd
@@ -0,0 +1,17 @@
+@echo off 
+
+set version=%~1
+echo compiling..
+call gulp compile
+
+echo adding new files..
+call git add .
+
+echo commiting tag..
+call git commit -m "%version%"
+
+echo creating tag %version%..
+call git tag -a %version% -m "%version%"
+
+echo pushing..
+call git push origin %version%
